1b108ad0b608c7195586dc3aece0c012748c3df1
Jay Ravi <jaykumarark@gmail.com>
2023-04-27 20:59:54 -0700

    Use fstream to write pixel data to file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <fstream> 
 #include "vec3.h"
 #include "color.h"
+#include <filesystem>
+
+namespace fs = std::filesystem; 
 
 int main()
 {
-    int const imageWidth = 256; 
-    int const imageHeight = 256; 
+    int const imageWidth = 1024; 
+    int const imageHeight = 768; 
 
-    std::cout<<"P3\n" <<imageWidth<< ' ' <<imageHeight<<"\n255\n";
+    fs::path output_path = fs::current_path() / "output.ppm";
+    std::ofstream imageFileObject (output_path, std::ios_base::out);
+
+    imageFileObject<<"P3\n" <<imageWidth<< ' ' <<imageHeight<<"\n255\n";
 
     for (int h = imageHeight - 1; h >=0; --h){
-        std::cerr << "\rScanlines remaining: " << h << ' ' << std::flush;
-
         for(int w = 0; w < imageWidth; ++w){
-            // auto r = double (w) / (imageWidth - 1);
-            // auto g = ;
-            // auto b = 0.25; 
-            color pixelColor (double (w) / (imageWidth - 1), double (h) / (imageHeight - 1), 0.25); 
-            WriteColor(std::cout, pixelColor); 
+            auto r = double (w) / (imageWidth - 1);
+            auto g = double (h) / (imageHeight - 1);
+            auto b = 0.25; 
+            color pixelColor (r, g, b); 
+            WriteColor(imageFileObject, pixelColor); 
         } 
     }
+    imageFileObject.close(); 
     std::cerr<<"\nDone.\n";
     return 0; 
 }
