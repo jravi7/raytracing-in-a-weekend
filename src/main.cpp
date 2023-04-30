c6d9988532cf36f4065db8fc6f46f175cb3c8c1b
Jay Ravi <jaykumarark@gmail.com>
2023-04-30 10:27:56 -0700

    setting up camera

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,28 @@
 #include <fstream> 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 #include <filesystem>
 
 namespace fs = std::filesystem; 
 
 int main(int argc, char** argv)
 {
-    int const imageWidth = 1024; 
-    int const imageHeight = 768; 
+    //image properties
+    float const aspectRatio = 16/9; 
+    int const imageHeight = 1080; 
+    int const imageWidth = imageHeight * aspectRatio; 
+
+    //camera properties
+    int viewportHeight = 2.0f; 
+    int viewportWidth = viewportHeight * aspectRatio; 
+    float focalLength = 1.0; 
+
+    auto origin = point3();
+    auto horizontal = vec3(viewportWidth, 0, 0); 
+    auto vertical = vec3(0, viewportHeight, 0); 
+    auto lowerLeft = origin - horizontal/2 - vertical/2 - vec3(0,0,focalLength);
+    
 
     fs::path output_path = fs::current_path() / "output.ppm";
     std::ofstream imageFileObject (output_path, std::ios_base::out);
@@ -20,7 +34,7 @@ int main(int argc, char** argv)
         for(int w = 0; w < imageWidth; ++w){
             auto r = double (w) / (imageWidth - 1);
             auto g = double (h) / (imageHeight - 1);
-            auto b = 0.25; 
+            auto b = 0.25;  
             color pixelColor (r, g, b); 
             WriteColor(imageFileObject, pixelColor); 
         } 
